b362345c9b3991d39e8746e6dbfdfc57a6eb3fd2
17thFox <elisabeta.vulpe@gmail.com>
2016-12-06 17:04:36 +0200

    made client-server requests

--- express-framework-exercises/myapp/app.js
+++ express-framework-exercises/myapp/app.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.use('/', express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-})
+});
 
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
 
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
-})
+app.post('/search/movie', function (req, res) {
+  res.send('You searched for a movie using the term: ' + req.body.movie);
+});
 
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
-})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-})
+});
