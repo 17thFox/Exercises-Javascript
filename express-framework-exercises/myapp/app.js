e9d7c4c3f2500ea2cc44c0867381ce7e2e2130af
17thFox <elisabeta.vulpe@gmail.com>
2016-12-12 15:22:10 +0200

    cached in an object my response

--- express-framework-exercises/myapp/app.js
+++ express-framework-exercises/myapp/app.js
@@ -2,26 +2,61 @@ var express = require('express');
 var app = express();
 var qhttp = require('q-io/http');
 
+var cachingObject = {
+    'myNewMovie': '',
+    'myLastMovie': '',
+    'myResponseJSON': {}
+};
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 
 app.use('/', express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
 
-app.post('/search/movie', function (req, res) {
-	var omdbapiURL = 'https://www.omdbapi.com/?s=' + encodeURIComponent(req.body.movie).replace(/%20/g, "+");
+app.post('/search/movie', function(req, res) {
 
-	qhttp.read(omdbapiURL).then(function(json){
-		var responseJSON = JSON.parse(json);
-		res.send(responseJSON['Search']);
-	}).then(null,console.error).done();
+    cachingObject.myNewMovie = req.body.movie;
+
+    if (cachingObject.myLastMovie === '' || cachingObject.myLastMovie !== cachingObject.myNewMovie) {
+
+        var omdbapiURL = 'https://www.omdbapi.com/?s=' + encodeURIComponent(req.body.movie).replace(/%20/g, "+");
+
+        console.log("went to: " + omdbapiURL);
+
+        qhttp.read(omdbapiURL).then(function(json) {
+            var responseJSON = JSON.parse(json);
+            cachingObject.myLastMovie = req.body.movie;
+            cachingObject.myResponseJSON = responseJSON;
+            res.send(responseJSON['Search']);
+        }).then(null, console.error).done();
+    } else {
+
+        console.log("got this: " + cachingObject.myResponseJSON['Search']);
+        res.send(cachingObject.myResponseJSON['Search']);
+    }
+
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
+
+
+
+
+// daca scriu abc, dup-aia scriu repede si d si il sterg
+// sa nu caute iar dupa abc, daca a cautat prima data
+// pe server la fiecare raspuns primit de la omdb, 
+//sa cache-uiasca search string-ul si raspunsul intr-o 
+//variabila globala
+// si apoi, daca are deja raspunsul la search-ul cerut, 
+//backend-ul sa il dea direct fara sa mai mearga 
+//pana la omdb
